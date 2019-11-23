2cf1490bb4ac78ba167bb0372d52f48449c99267
Artem Smirnov <artem.e.smirnov@gmail.com>
2019-11-23 22:42:54 +0500

    Add json properties annotations

--- src/main/java/com/miro/widget/api/model/dto/WidgetDto.java
+++ src/main/java/com/miro/widget/api/model/dto/WidgetDto.java
@@ -1,5 +1,7 @@
 package com.miro.widget.api.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.miro.widget.api.model.entity.Widget;
 import lombok.*;
 
@@ -13,18 +15,25 @@ import java.util.UUID;
 @EqualsAndHashCode
 public class WidgetDto {
 
+    @JsonProperty(value = "id")
     private UUID id;
 
+    @JsonProperty(value = "xcoordinate")
     private long xCoordinate;
 
+    @JsonProperty(value = "ycoordinate")
     private long yCoordinate;
 
+    @JsonProperty(value = "zindex")
     private Long zIndex;
 
+    @JsonProperty(value = "width")
     private long width;
 
+    @JsonProperty(value = "height")
     private long height;
 
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private Date modifiedAt;
 
     public static WidgetDto fromEntity(Widget entity) {
