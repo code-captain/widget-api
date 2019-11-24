2a2a946fca634a07191dba7f60e9b1cd8660a5e5
Artem Smirnov <artem.e.smirnov@gmail.com>
2019-11-24 15:37:14 +0500

    Fix error response format

--- src/main/java/com/miro/widget/api/handler/MethodArgumentNotValidExceptionHandler.java
+++ src/main/java/com/miro/widget/api/handler/MethodArgumentNotValidExceptionHandler.java
@@ -6,11 +6,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.*;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @ControllerAdvice
@@ -27,6 +29,7 @@ public class MethodArgumentNotValidExceptionHandler extends ResponseEntityExcept
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", new Date());
         body.put("status", status.value());
+        body.put("error", status.getReasonPhrase());
 
         List<String> errors = ex.getBindingResult()
                 .getFieldErrors()
@@ -34,7 +37,7 @@ public class MethodArgumentNotValidExceptionHandler extends ResponseEntityExcept
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .collect(Collectors.toList());
 
-        body.put("errors", errors);
+        body.put("messages", errors);
 
         return new ResponseEntity<>(body, headers, status);
 
